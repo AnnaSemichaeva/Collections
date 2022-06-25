565da7e7949627550f2ff8ccdcd4e8650f6068e8
AnnaSemichaeva <tdhnss@gmail.com>
2022-06-26 00:04:41 +0300

    minor changes

--- Playgrounds/ArraysPlayground.swift
+++ Playgrounds/ArraysPlayground.swift
@@ -11,4 +11,9 @@ public func arraysPlayground() {
     
     //1) вывести массив в виде массива
     
+    print("***-1) вывести массив в виде массива-***")
+    var someInts = [1,2,3,4,5,22,53,-5]
+    print(someInts)
+    
+    //2)
 }
