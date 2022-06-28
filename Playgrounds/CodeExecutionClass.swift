0c240e81cc8e796406e4acb62150fa6f52ae94a4
AnnaSemichaeva <tdhnss@gmail.com>
2022-06-28 20:28:43 +0300

    minor changes

---
+++ Playgrounds/CodeExecutionClass.swift
@@ -0,0 +1,22 @@
+//
+//  CodeExecutionClass.swift
+//  Collections
+//
+//  Created by macuser on 6/28/22.
+//
+
+import Foundation
+
+public class CodeExecutionCode {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Playgrounds/JustPlayground.swift
@@ -0,0 +1,30 @@
+//
+//  JustPlayground.swift
+//  Collections
+//
+//  Created by macuser on 6/28/22.
+//
+
+import Foundation
+
+public func justPlayground() {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
